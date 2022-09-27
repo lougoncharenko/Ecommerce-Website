315a010edb5fb28a9fbc71bedec843c96ac1251c
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-09-27 19:39:06 -0400

    updated desc

--- data.js
+++ data.js
@@ -2,39 +2,38 @@ const data = [{
     "id": 1,
     "name": "Birds of Paradise",
     "image": "/images/bird of paradise.jpeg",
-    "desc": "Happiness lies in the joy of achievement and the thrill of creative effort.",
+    "desc": "Birds of Paradise",
     "price": 5.99
   }, {
     "id": 2,
     "name": 'Green Pothos',
     "image": 'images/green pothos.jpeg',
-    "desc": "Tears are words that can’t be explained.",
+    "desc": "Green Pothos",
     "price": 5.99
   }, {
     "id": 3,
     "name": 'Money Plant Tree',
     "image": "images/money tree plant.jpeg",
-    "desc": "Anger is like a storm rising up from the bottom of your consciousness. When you feel it coming, turn your focus to your breath.",
+    "desc": "Money Plant Tree",
     "price": 5.99
   }, {
     "id": 4,
     "name": 'Monstera',
     "image": "images/monstera.jpeg",
-    "desc": "To be calm is the highest achievement of the self.",
+    "desc": "Monstera",
     "price": 5.99
   }, {
     "id": 5,
     "name": 'Peperomia',
     "image": "images/peperomia.jpeg",
-    "desc": "I have no special talents. I am only passionately curious.",
+    "desc": "Peperomia",
     "price": 5.99
   }, {
     "id": 6,
     "name": 'Snake Plant',
     "image": "images/snakeplant.jpeg",
-    "desc": "Not all disgust need produce symptoms of nausea, nor all nausea mark the presence of disgust.",
+    "desc": "Snake Plant",
     "price": 5.99
   }]
 
   export default data;
-  
